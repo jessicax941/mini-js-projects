78333cccfab2e1c25ee6c5b6b43bc95696a6ada8
Jessica Phua Shu Xin <jessicax941@gmail.com>
2021-12-26 12:49:55 +0800

    toggle dark or light mode based on background colour

--- colour-flipper/app.js
+++ colour-flipper/app.js
@@ -14,6 +14,8 @@ const simpleColours = [
 	'rgb(116, 185, 255)',
 	'rgb(129, 236, 236)',
 	'rgb(162, 155, 254)',
+	'rgb(140, 39, 30)',
+	'rgb(91, 80, 122)',
 ];
 
 // startup
@@ -22,13 +24,14 @@ let simpleNavButton = document.getElementById('simple-gen');
 let hexNavButton = document.getElementById('hex-gen');
 simpleNavButton.classList.toggle(navButtonSelectedClass);
 let genType = 'simple'; // simple or hex
+let generateButton = document.getElementById('generate-btn');
 
 fillColour();
 
 // handle button clicks
 simpleNavButton.addEventListener('click', () => handleNavButtonClick('simple'));
 hexNavButton.addEventListener('click', () => handleNavButtonClick('hex'));
-document.getElementById('generate-btn').addEventListener('click', fillColour);
+generateButton.addEventListener('click', fillColour);
 
 // type is either 'simple' or 'hex'
 function handleNavButtonClick(type) {
@@ -37,8 +40,11 @@ function handleNavButtonClick(type) {
 		return;
 	}
 
+	genType = type;
+
 	if (type === 'simple') {
 		if (!simpleNavButton.classList.contains(navButtonSelectedClass)) {
+			fillColour();
 			simpleNavButton.classList.add(navButtonSelectedClass);
 		}
 
@@ -48,6 +54,7 @@ function handleNavButtonClick(type) {
 	} else {
 		// type is hex
 		if (!hexNavButton.classList.contains(navButtonSelectedClass)) {
+			fillColour();
 			hexNavButton.classList.add(navButtonSelectedClass);
 		}
 
@@ -55,29 +62,46 @@ function handleNavButtonClick(type) {
 			simpleNavButton.classList.remove(navButtonSelectedClass);
 		}
 	}
-
-	genType = type;
 }
 
 function fillColour() {
 	let rgbColour;
-	let hexColour;
 
 	if (genType === 'simple') {
 		rgbColour = getRandomPresetColour(simpleColours);
-		hexColour = rgbToHex(rgbColour);
 	} else {
 		// type is hex
-		hexColour = generateRandomColour();
-		console.log(hexColour);
+		rgbColour = generateRandomColour();
 	}
-	document.body.style.background = rgbColour ? rgbColour : hexColour;
+	let hexColour = rgbToHex(rgbColour);
+	document.body.style.background = rgbColour;
 
 	// display colour by hex and its colour name
 	let colourMatch = ntc.name(hexColour);
 	let colourName = colourMatch[1];
 	document.getElementById('colour-hex').innerHTML = hexColour;
 	document.getElementById('colour-name').innerHTML = colourName;
+
+	// toggle light or dark mode based on rgb colour
+	let isLightMode = isLightColour(rgbColour);
+	let mainSelection = document.querySelector('main');
+	if (isLightMode) {
+		if (!mainSelection.classList.contains('light-mode')) {
+			mainSelection.classList.add('light-mode');
+		}
+
+		if (mainSelection.classList.contains('dark-mode')) {
+			mainSelection.classList.remove('dark-mode');
+		}
+	} else {
+		if (!mainSelection.classList.contains('dark-mode')) {
+			mainSelection.classList.add('dark-mode');
+		}
+
+		if (mainSelection.classList.contains('light-mode')) {
+			mainSelection.classList.remove('light-mode');
+		}
+	}
 }
 
 function getRandomPresetColour(set) {
@@ -90,26 +114,18 @@ function getRandomPresetColour(set) {
 	return randColour;
 }
 
+// generate random rgb colour
 function generateRandomColour() {
-	let hex = '#';
-	for (let i = 0; i < 6; i++) {
-		let randNum = Math.floor(Math.random() * 16);
-		let randHex = randNum.toString(16); // convert to hexadecimal
-		hex += randHex;
+	let rgb = [];
+	for (let i = 0; i < 3; i++) {
+		rgb[i] = Math.floor(Math.random() * 256);
 	}
-	return hex.toUpperCase();
+	return `rgb(${rgb[0]}, ${rgb[1]}, ${rgb[2]})`;
 }
 
 // convert rgb string 'rgb(r, g, b) to hex string '#xxxxxx'
 function rgbToHex(rgb) {
-	rgb = rgb.substring(4, rgb.length - 1);
-	let components = rgb.split(', ');
-
-	// convert rgb to numbers
-	for (let i = 0; i < components.length; i++) {
-		let comp = components[i];
-		components[i] = parseInt(comp, 10);
-	}
+	let components = getRgbComponents(rgb);
 
 	// convert the numbers to hexadecimal strings and combine them
 	let hex = components.reduce((prev, curr) => {
@@ -121,4 +137,30 @@ function rgbToHex(rgb) {
 	}, '');
 
 	return '#' + hex.toUpperCase();
+}
+
+// gets rgb components as a number array [r, g, b]
+function getRgbComponents(rgb) {
+	rgb = rgb.substring(4, rgb.length - 1);
+	let components = rgb.split(', ');
+
+	// convert rgb to numbers
+	for (let i = 0; i < components.length; i++) {
+		let comp = components[i];
+		components[i] = parseInt(comp, 10);
+	}
+
+	return components;
+}
+
+// returns true if light colour, false if dark colour
+function isLightColour(rgb) {
+	// taken from https://awik.io/determine-color-bright-dark-using-javascript/
+	let components = getRgbComponents(rgb);
+	let r = components[0];
+	let g = components[1];
+	let b = components[2];
+
+	let hsp = Math.sqrt(0.299 * (r * r) + 0.587 * (g * g) + 0.114 * (b * b));
+	return hsp > 127.5;
 }
